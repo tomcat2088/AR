a7bd0977dd523ad0dea96bf2ea0afb5819789b7c
tomcat2088 <tomcat1991@126.com>
2016-02-19 17:27:59 +0800

    all

---
+++ OnPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifDemo
+//
+//  Created by wangyang on 16/1/25.
+//  Copyright © 2016年 wangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
